f9aea4290acad74fd3b77010edf143533684f53a
VegaRuth <ruth222@gmail.com>
2016-06-10 13:55:02 -0500

    Ahora si

---
+++ js/main.js
@@ -0,0 +1,41 @@
+//Variables Globales //
+
+//Funciones
+function presionarEnter input(ele) {
+    if(event.keyCode == 13) {
+        //Agregar mensaje       
+    }
+};
+
+//Inicio//
+$(document).ready(function(){
+var mensaje=$("#inputTextMensaje").val();//valor de Input
+
+
+
+
+
+
+$("#inputTextMensaje").val("");//limpiarInput
+
+	$("#inputTextMensaje").keydown(function (enviarMensaje){
+		if ( event.keyCode === 13) {
+			//AgregarMEnsaje
+		}
+	});
+
+function presionarEnter input(ele) {
+    if(event.keyCode == 13) {
+        //Agregar mensaje       
+    }
+}
+
+
+
+
+
+
+});//Cierre ready
+
+
+
